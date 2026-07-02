4286c91b0c4951e27511d54ebe59a73ac92cece4
Sarthak S Kumar <sarthakskumar7@gmail.com>
2026-07-02 06:33:09 +0530

    feat(types): add index and inputs implementation

--- src/types/index.ts
+++
@@ -1,9 +0,0 @@
-﻿// Auto-generated Shopify Admin GraphQL API 2026-04 - all type definitions
-// Do not edit manually - regenerate with parse_types.ps1
-export * from './enums';
-export * from './interfaces';
-export * from './inputs';
-export * from './objects';
-export * from './connections';
-export * from './unions';
-export * from './payloads';
